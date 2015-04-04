9462c54c09f0a957c89f0b54fb1a9fe077e66aae
juliolemus <juliolemus165@yahoo.com>
2015-04-04 08:22:23 -0700

    added a few things

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :uploads
+
+  resources :comments
+
   resources :posts do 
     resources :comments
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404110849) do
+ActiveRecord::Schema.define(version: 20150404142450) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20150404110849) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "uploads", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
