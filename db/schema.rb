c318649a02be4613f14bab6c35d6aed3c6d58c65
Charles Weng <charlesweng@Charless-MacBook-Pro.local>
2015-04-04 07:44:27 -0700

    simple signup and login made for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404110849) do
+ActiveRecord::Schema.define(version: 20150404120555) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20150404110849) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
